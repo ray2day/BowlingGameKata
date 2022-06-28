053cb0877c771061c6f7f00876183365d83f241d
Raymond van Hoorn <vanhoornraymond@gmail.com>
2022-06-28 16:45:33 +0200

    Add project files.

---
+++ BowlingGameKata/BowlingGameTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xunit;
+
+namespace BowlingGameKata
+{
+    /// <summary>
+    /// HINTS:
+    /// At a minimum you'll want to use the following tests:
+    /// Test for all gutterballs(knocked down 0 pins for the entire game.Total Score == 0)
+    /// Test for knocking down 1 pin each time(Total Score == 20)
+    /// Test a perfect game(knocked down 10 pins each throw, Total Score == 300)
+    /// </summary>
+    public class BowlingGameTest
+    {
+        private Game g;
+
+        public BowlingGameTest()
+        {
+            g = new Game();
+        }
+
+        [Fact]
+        public void TestBowlingGameClass()
+        {
+            Assert.IsType<Game>(g);
+        }
+
+        [Fact]
+        public void TestGutterGame()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(0);
+            }
+
+            Assert.Equal(0, g.Score);
+        }
+     
+        //public void TestAllOnes()
+
+        //public void TestOneSpare()
+
+        //public void TestOneStrike()
+
+        // public void TestPerfectGame()
+
+        // etc..
+
+    }
+}
---
+++ BowlingGameKata/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGameKata
+{
+    public class Game
+    {
+        public int Score { get; set; }
+
+        public void Roll(int pins)
+        {
+            
+        }
+    }
+}
